ecac03a2c6e9c082f7834d9a5c514ec9b7eac6af
Matthieu Lempereur <matt.lempereur@gmail.com>
2020-12-16 16:33:44 +0100

    FunctionComponent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,37 +2,31 @@ import React from "react";
 import { Profile } from "../models/profile";
 import "./App.scss";
 
-interface State extends Profile {}
+const App: React.FunctionComponent<{}> = {} => {
+  const [fullName, setFullName] = useState("Stéphane Jean");
+  const [title, setTitle] = useState("Architect");
+  const [country, setCountry] = useState("New York, USA");
+  const [imageUrl, setImageUrl] = useState("https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_1280.png");
 
-export class App extends React.Component<{}, State> {
-  state = {} as State;
-
-  public componentDidMount() {
+  useEffect(() => {
     if (chrome && chrome.tabs) {
       chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
         const tab = tabs[0];
         chrome.tabs.sendMessage(tab.id || 0, { from: "popup", subject: "getFullName" }, response => {
-          this.setState({
-            fullName: response.fullName,
-            title: response.title,
-            country: response.country,
-            image: response.image,
-          });
+          setFullName(response.fullName);
+          setTitle(response.title);
+          setCountry(response.country);
+          setImageUrl(response.imgeUrl);
         });
       });
     }
-  }
+  });
 
-  render() {
-    return (
+  return (
     <div className="app">
-      <div>{this.state.fullName}</div>
-      <div>{this.state.title}</div>
-      <div>{this.state.country}</div>
-      <img src={this.state.image} alt="Image profile"></img>
-    </div>;
+      <User fullName={fullName} title={title} country={country} imageUrl={imageUrl}></User>
+    </div>
     );
-  }
-}
+};
 
 export default App;
---
+++ src/components/user/user.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Profile } from "../../models/profile";
+
+interface IProps extends Profile {}
+
+const User: React.FunctionComponent<IProps> = props => {
+    const {fullName, title, country, imageUrl} = props;
+
+    return (
+        <div>
+            <div>{fullName}</div>
+            <div>{title}</div>
+            <div>{country}</div>
+            <img src={imageUrl}></img>
+        </div>
+    );
+};
+
+export default User;
